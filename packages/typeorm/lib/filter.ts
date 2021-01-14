49cf6874fdb19c7a633ad226d5f87720525c4d8f
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-01-14 14:24:49 -0300

    add filter

--- packages/typeorm/lib/filter.ts
+++ packages/typeorm/lib/filter.ts
@@ -1,51 +1,51 @@
-import { Between, FindOperator, Raw } from 'typeorm';
-import moment from 'moment';
+// import { Between, FindOperator, Raw } from 'typeorm';
+// import moment from 'moment';
 
-import { RequestGetAllInterface } from '../../interfaces/pagination';
+// import { RequestGetAllInterface } from '../../interfaces/pagination';
 
-export const dynamicFilter = <T>(
-    data: T & RequestGetAllInterface,inactive = false, tenantid?: string,
-): Record<string, unknown> => {
-    const { dateFilter, startDateFilter, endDateFilter } = data;
+// export const dynamicFilter = <T>(
+//     data: T & RequestGetAllInterface,inactive = false, tenantid?: string,
+// ): Record<string, unknown> => {
+//     const { dateFilter, startDateFilter, endDateFilter } = data;
 
-    const queryParams = data;
+//     const queryParams = data;
 
-    if (queryParams.showInactive) delete queryParams.showInactive;
-    if (queryParams.withPagination) delete queryParams.withPagination;
-    if (queryParams.page) delete queryParams.page;
-    if (queryParams.size) delete queryParams.size;
-    if (queryParams.sortParam) delete queryParams.sortParam;
-    if (queryParams.sortOrder) delete queryParams.sortOrder;
-    if (queryParams.dateFilter) delete queryParams.dateFilter;
-    if (queryParams.startDateFilter) delete queryParams.startDateFilter;
-    if (queryParams.endDateFilter) delete queryParams.endDateFilter;
+//     if (queryParams.showInactive) delete queryParams.showInactive;
+//     if (queryParams.withPagination) delete queryParams.withPagination;
+//     if (queryParams.page) delete queryParams.page;
+//     if (queryParams.size) delete queryParams.size;
+//     if (queryParams.sortParam) delete queryParams.sortParam;
+//     if (queryParams.sortOrder) delete queryParams.sortOrder;
+//     if (queryParams.dateFilter) delete queryParams.dateFilter;
+//     if (queryParams.startDateFilter) delete queryParams.startDateFilter;
+//     if (queryParams.endDateFilter) delete queryParams.endDateFilter;
 
-    const queryDataEntries = Object.entries(data);
+//     const queryDataEntries = Object.entries(data);
 
-    let query: Record<string, unknown> = {};
+//     let query: Record<string, unknown> = {};
 
-    for (const [key, value] of queryDataEntries) {
-        query = {
-            ...query,
-            [key]: Raw(alias => `CAST(${alias} AS VARCHAR) ILIKE '%${value}%'`),
-        };
-    }
+//     for (const [key, value] of queryDataEntries) {
+//         query = {
+//             ...query,
+//             [key]: Raw(alias => `CAST(${alias} AS VARCHAR) ILIKE '%${value}%'`),
+//         };
+//     }
 
-    if (dateFilter && startDateFilter && endDateFilter)
-        query[dateFilter] = betweenDataFilter(startDateFilter, endDateFilter);
+//     if (dateFilter && startDateFilter && endDateFilter)
+//         query[dateFilter] = betweenDataFilter(startDateFilter, endDateFilter);
 
-    if (!inactive && tenantid) query.active = true;
-    if (tenantid) query.tenantid = tenantid;
+//     if (!inactive && tenantid) query.active = true;
+//     if (tenantid) query.tenantid = tenantid;
 
-    return query;
-};
+//     return query;
+// };
 
-export const betweenDataFilter = (
-    startDate: string,
-    endDate: string,
-): FindOperator<moment.Moment> => {
-    return Between(
-        moment(startDate, 'YYYY-MM-DD').subtract(1, 'days'),
-        moment(endDate, 'YYYY-MM-DD').add(1, 'days'),
-    );
-};
+// export const betweenDataFilter = (
+//     startDate: string,
+//     endDate: string,
+// ): FindOperator<moment.Moment> => {
+//     return Between(
+//         moment(startDate, 'YYYY-MM-DD').subtract(1, 'days'),
+//         moment(endDate, 'YYYY-MM-DD').add(1, 'days'),
+//     );
+// };
--- packages/typeorm/lib/index.ts
+++ packages/typeorm/lib/index.ts
@@ -1,40 +1,40 @@
-// Response GetALl
+// // Response GetALl
 
-import { buildSortParams, getPaginationParams } from '../paginate';
-import { OptionsTypeOrmGetAll, RequestGetAllInterface } from './interfaces';
-import { dynamicFilter } from './dynamicFilter';
+// import { buildSortParams, getPaginationParams } from '../paginate';
+// import { OptionsTypeOrmGetAll, RequestGetAllInterface } from './interfaces';
+// import { dynamicFilter } from './dynamicFilter';
 
-export const sum = (x: number, y: number): number => {
-    return x + y;
-};
+// export const sum = (x: number, y: number): number => {
+//     return x + y;
+// };
 
-export const buildResponseGetAll = <T>(
-    queryParams: T & RequestGetAllInterface,
-    inactive = false,
-    tenantid?: string,
-): OptionsTypeOrmGetAll => {
-    const { take, skip } = getPaginationParams(queryParams);
-    const { sortParam, sortOrder } = buildSortParams(queryParams);
+// export const buildResponseGetAll = <T>(
+//     queryParams: T & RequestGetAllInterface,
+//     inactive = false,
+//     tenantid?: string,
+// ): OptionsTypeOrmGetAll => {
+//     const { take, skip } = getPaginationParams(queryParams);
+//     const { sortParam, sortOrder } = buildSortParams(queryParams);
 
-    const query = { ...queryParams };
+//     const query = { ...queryParams };
 
-    const where = tenantid
-        ? dynamicFilter(query, inactive, tenantid)
-        : dynamicFilter(query, inactive);
+//     const where = tenantid
+//         ? dynamicFilter(query, inactive, tenantid)
+//         : dynamicFilter(query, inactive);
 
-    const build: OptionsTypeOrmGetAll = {
-        where,
-        order: { [sortParam]: `${sortOrder}` },
-        take,
-        skip,
-        orderBy: {
-            columnName: sortParam,
-            order: <'DESC' | 'ASC'>`${sortOrder}`,
-        },
-    };
+//     const build: OptionsTypeOrmGetAll = {
+//         where,
+//         order: { [sortParam]: `${sortOrder}` },
+//         take,
+//         skip,
+//         orderBy: {
+//             columnName: sortParam,
+//             order: <'DESC' | 'ASC'>`${sortOrder}`,
+//         },
+//     };
 
-    if (!inactive) build.where.active = true;
-    if (tenantid) build.where.tenantid = tenantid;
+//     if (!inactive) build.where.active = true;
+//     if (tenantid) build.where.tenantid = tenantid;
 
-    return build;
-};
+//     return build;
+// };
